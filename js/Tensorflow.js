29ddf7a54b0359faff1eaed25d9151923949d609
Sujee <sujeethan.vigneswaran@gmail.com>
2018-09-15 14:34:25 -0400

    YAY!!!

--- js/Tensorflow.js
+++ js/Tensorflow.js
@@ -1,83 +1,30 @@
-let featureExtractor;
 let classifier;
 let video;
-let loss;
-let banana1Images = 0;
-let banana2Images = 0;
-let banana3Images = 0;
-let appleImages = 0;
 
 function setup() {
   noCanvas();
-  // Create a video element
+  // Create a camera input
   video = createCapture(VIDEO);
-  // Append it to the videoContainer DOM element
-  video.parent('videoContainer');
-  // Extract the already learned features from MobileNet
-  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
-  // Create a new classifier using those features and give the video we want to use
-  classifier = featureExtractor.classification(video, videoReady);
-  // Create the UI buttons
-  setupButtons();
+  // Initialize the Image Classifier method with MobileNet and the video as the second argument
+  classifier = ml5.imageClassifier('MobileNet', video, modelReady);  
 }
 
-// A function to be called when the model has been loaded
 function modelReady() {
-  select('#modelStatus').html('Base Model (MobileNet) loaded!');
+  // Change the status of the model once its ready
+  select('#status').html('Model Loaded');
+  // Call the classifyVideo function to start classifying the video
+  classifyVideo();
 }
 
-// A function to be called when the video has loaded
-function videoReady () {
-  select('#videoStatus').html('Video ready!');
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.predict(gotResult);
 }
 
-
-// Classify the current frame.
-function classify() {
-  classifier.classify(gotResults);
-}
-
-// A util function to create UI buttons
-function setupButtons() {
-	
-	/**
-	classifier.addImage('classifier/banana1/img', 'banana1');
-	select('#amountOfBanana1Images').html(banana1Images++);
-	**/
-		 
-	classifier.addImage('image/banana2/15.jpeg', 'banana2');
-	select('#amountOfBanana2Images').html(banana2Images++);
-	
-	/**
-	classifier.addImage('image/banana3/img', 'banana3');
-	select('#amountOfBanana3Images').html(banana3Images++);
-	
-	classifier.addImage('image/apple/img', 'apple');
-	select('#amountOfAppleImages').html(AppleImages++);
-	
-	**/
-
-  // Train Button
-  train = select('#train');
-	classifier.train(function(lossValue) {
-      if (lossValue) {
-        loss = lossValue;
-        select('#loss').html('Loss: ' + loss);
-      } else {
-        select('#loss').html('Done Training! Final Loss: ' + loss);
-      }
-    });
-
-  // Predict Button
-  buttonPredict = select('#buttonPredict');
-  buttonPredict.mousePressed(classify);
-}
-
-// Show the results
-function gotResults(err, result) {
-  if (err) {
-    console.error(err);
-  }
-  select('#result').html(result);
-  classify();
+// When we get a result
+function gotResult(err, results) {
+  // The results are in an array ordered by probability.
+  select('#result').html(results[0].className);
+  select('#probability').html(nf(results[0].probability, 0, 2));
+  classifyVideo();
 }
